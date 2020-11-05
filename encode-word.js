938ec95586240785310ef202220ebb5bb25a7494
fernando duenas <fernando39615@aim.com>
2020-11-05 11:48:36 -0600

    commit

--- encode-word.js
+++ encode-word.js
@@ -6,9 +6,35 @@ const encodeWord = function(word) {
     let result = '';
     for (const char of word) {
         for (const emoji of emojis) {
-            if (emoji.letter === char) {
-                result += emoji.symbol;
+            if (emoji.letter === char.toLowerCase()) {
+                found = true;
+                result += emoji.symbol;//?
             }
         }
+        if(found === false){
+            result += char;//?
+        }
     }
+    return result;//?
 }
+
+// mapping it
+const encodeWordAlt = function(word){
+    word; //'dog'
+    const letters = word.split('');
+    letters // ['d', 'o', 'g']
+    const symbols = letters.map(function(char){
+        for (const emoji of emojis) {
+            if (emoji.letter === char.toLowerCase()) {
+                return emoji.symbol;
+            }
+        }
+        return char;//?
+    });
+    symbols; 
+    return symbols.join('')//?
+}
+
+encodeWord('dog')//?
+
+module.exports = encodeWord;
--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const translateWord = require('./translate-word.js');
 const encodeWord = require('./encode-word.js');
 
+const command = process.argv[2];
 
-const words = process.argv.slice(2);
-// const encoding = words.map(encodeWord);
-// console.log(encoding.join(' '));
-const translation = words.map(translateWord)
-console.log(translation.join(' '));
+switch(command){
+    case command === 'translate':
+        const words = process.argv.slice(3);
+        const translation = words.map(translateWord);
+        console.log(translation.join(' '));
+        break;
+
+    case command === 'encode':
+        const encoding = words.map(encodeWord);
+        console.log(encoding.join(' '));
+        break;
+
+}
